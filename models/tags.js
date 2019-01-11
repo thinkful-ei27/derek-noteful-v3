45f81537760e219fc4a9a4e067c8a11162aaade4
Derek Houck <derek@derekhouck.com>
2019-01-11 10:23:59 -0800

    Create Tag model

---
+++ models/tags.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const tagSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true }
+});
+
+tagSchema.set('timestamps', true);
+
+tagSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports = mongoose.model('Tag', tagSchema);
